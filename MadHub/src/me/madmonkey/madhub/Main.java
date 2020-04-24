3b78111c2ced932dd8e64fd19544721b6494250c
MadMonkey808 <59536765+MadMonkey808@users.noreply.github.com>
2020-04-23 19:22:46 -0700

    Intial Commit: Added Main.class and .gitignore File

---
+++ MadHub/src/me/madmonkey/madhub/Main.java
@@ -0,0 +1,14 @@
+package me.madmonkey.madhub;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    public void onEnable(){
+
+    }
+
+    public void onDisable(){
+
+    }
+}
