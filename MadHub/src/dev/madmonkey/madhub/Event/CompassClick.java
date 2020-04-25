391357eca94e45bfd54aecc628f148d9850cb706
MadMonkey808 <59536765+MadMonkey808@users.noreply.github.com>
2020-04-25 11:31:39 -0700

    Added CompassClick on Event. Continued CompassSelector class.

---
+++ MadHub/src/dev/madmonkey/madhub/Event/CompassClick.java
@@ -0,0 +1,6 @@
+package dev.madmonkey.madhub.Event;
+
+import org.bukkit.event.Listener;
+
+public class CompassClick implements Listener {
+}
--- MadHub/src/dev/madmonkey/madhub/Utils/CompassSelector.java
+++ MadHub/src/dev/madmonkey/madhub/Utils/CompassSelector.java
@@ -1,21 +1,52 @@
 package dev.madmonkey.madhub.Utils;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
-public class CompassSelector {
+import java.util.ArrayList;
 
-    public CompassSelector (Player player){
+public class CompassSelector implements Listener {
 
-        Inventory inv = Bukkit.createInventory(player, 27, ChatFormat.color("&b&lServer"));
+    public CompassSelector(Player player){
 
-        ItemStack survival = new ItemStack(Material.GRASS, 1);
+        String survivalist =  ChatFormat.color("&aOnline: ") + player.getServer().getOnlinePlayers() + ChatFormat.color("&a / ") + Bukkit.getServer().getMaxPlayers();
+
+        Inventory inv = Bukkit.createInventory(null, 27, ChatFormat.color("&b&lServer"));
+
+        ItemStack empty = new ItemStack(Material.BLACK_STAINED_GLASS_PANE, 1);
+        ItemMeta emptyM = empty.getItemMeta();
+        emptyM.setDisplayName(ChatFormat.color(" "));
+        empty.setItemMeta(emptyM);
+
+        ItemStack survival = new ItemStack(Material.GRASS_BLOCK, 1);
         ItemMeta survivalM = survival.getItemMeta();
+        ArrayList<String>Lore = new ArrayList<String>();
         survivalM.setDisplayName(ChatFormat.color("&a&lSurvival"));
         survival.setItemMeta(survivalM);
+        Lore.add(survivalist);
+        survivalM.setLore(Lore);
+
+        ItemStack prison = new ItemStack(Material.IRON_BARS, 1);
+        ItemMeta prisonM = survival.getItemMeta();
+        prisonM.setDisplayName(ChatFormat.color("&7&lPrison"));
+        prison.setItemMeta(prisonM);
+
+        ItemStack skyblock = new ItemStack(Material.CHEST, 1);
+        ItemMeta skyblockM = survival.getItemMeta();
+        skyblockM.setDisplayName(ChatFormat.color("&b&lSkyblock"));
+        skyblock.setItemMeta(skyblockM);
+
+        inv.setItem(11, survival);
+        inv.setItem(13, prison);
+        inv.setItem(15, skyblock);
+
+
+        player.openInventory(inv);
     }
 }
