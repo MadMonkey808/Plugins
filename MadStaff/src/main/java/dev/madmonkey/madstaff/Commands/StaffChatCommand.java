0eedca5f5c4781d3cc8c8ab1e2bcadb21e1b00b0
MadMonkey808 <59536765+MadMonkey808@users.noreply.github.com>
2020-04-26 21:41:52 -0700

    Added MadStaff Plugin

---
+++ MadStaff/src/main/java/dev/madmonkey/madstaff/Commands/StaffChatCommand.java
@@ -0,0 +1,47 @@
+package dev.madmonkey.madstaff.Commands;
+
+import dev.madmonkey.madstaff.Main;
+import net.md_5.bungee.BungeeCord;
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.chat.TextComponent;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
+import net.md_5.bungee.api.plugin.Command;
+
+public class StaffChatCommand extends Command {
+
+    String prefix = ChatColor.AQUA + "[S]";
+
+    public StaffChatCommand() {
+        super("staffchat", "madstaffchat.use", "sc");
+    }
+
+    public void execute(CommandSender sender, String[] args) {
+        if (sender instanceof ProxiedPlayer) {
+            ProxiedPlayer p = (ProxiedPlayer)sender;
+            if (args.length == 0) {
+                if (p.hasPermission("bungee.staff")) {
+                    if (Main.inSc.contains(p)) {
+                        p.sendMessage(new TextComponent(prefix + ChatColor.GREEN + "StaffChat has been disabled!"));
+                        Main.inSc.remove(p);
+                    } else {
+                        Main.inSc.add(p);
+                        p.sendMessage(new TextComponent(prefix + ChatColor.GREEN + "StaffChat has been enabled!"));
+                    }
+                } else {
+                    p.sendMessage(new TextComponent(prefix + ChatColor.RED + "You do not have permissions to execute this command!"));
+                }
+            } else if (p.hasPermission("bungee.staff")) {
+                String msg = " ";
+                for (int i = 0; i < args.length; i++)
+                    msg = String.valueOf(msg) + args[i] + " ";
+                for (ProxiedPlayer staff : BungeeCord.getInstance().getPlayers()) {
+                    if (staff.hasPermission("bungee.staff"))
+                        staff.sendMessage(new TextComponent(prefix + ChatColor.BLUE + "[" + p.getServer().getInfo().getName() + ChatColor.BLUE + "]" + p.getDisplayName() + ChatColor.AQUA + ": " + msg));
+                }
+            } else {
+                p.sendMessage(new TextComponent(prefix + ChatColor.RED + "You do not have permissions to execute this command!"));
+            }
+        }
+    }
+}
---
+++ MadStaff/src/main/java/dev/madmonkey/madstaff/Main.java
@@ -0,0 +1,23 @@
+package dev.madmonkey.madstaff;
+
+import dev.madmonkey.madstaff.Commands.StaffChatCommand;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
+import net.md_5.bungee.api.plugin.Plugin;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main extends Plugin {
+
+    public static List<ProxiedPlayer> inSc = new ArrayList<>();
+
+    public void onEnable(){
+        getProxy().getPluginManager().registerCommand(this, new StaffChatCommand());
+        getLogger().info("StaffChat has been Enabled!");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("StaffChat has been Disabled!");
+    }
+}
